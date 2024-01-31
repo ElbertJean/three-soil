0b41725a899545b6614fd9d60d2833b302288602
Elbert Jean <elbertjeandossantos@gmail.com>
2024-01-31 10:55:05 -0300

    :sparkles: feat: Cards das áreas na HomePage

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,12 +1,42 @@
 import Layout from "../../components/layout/Layout";
+import styles from './HomePage.module.css';
+import area1 from '../../assets/area1.svg';
+import area2 from '../../assets/area2.svg';
 
 function HomePage(): JSX.Element {
 
     return(
         <>
             <Layout>
-                <div style={{ display:' flex', justifyContent: 'center', alignItems: 'center', height: '80vh'}}>
-                    <p style={{color: '#001633', fontWeight:' bold', fontSize: '50px'}}>Em breve, React Leaflet</p>
+                <div className={styles.container}>
+                    <div className={styles.containerCards}>
+                        <div className={styles.card}>
+                            <img src={area1} alt='área 1' className={styles.imageMap}/>
+                            <div className={styles.descriptionCard}>
+                                <h3 className={styles.titleCard}>Fazenda Recanto dos Pássaros</h3>
+                                <p className={styles.paragraphCard}>
+                                    Fazenda localizada na região oeste de São José dos Campos, sendo umas das 
+                                    principais áreas de plantio de milho da região.
+                                </p>
+                            </div>
+                            <div className={styles.divButton}>
+                                <button className={styles.buttonActive} onClick={()=>alert('auu')}>Acessar</button>
+                            </div>
+                        </div>
+                        <div className={styles.card}>
+                            <img src={area2} alt='área 2' className={styles.imageMap}/>
+                            <div className={styles.descriptionCard}>
+                                <h3 className={styles.titleCard}>Floresta preservada Campo da Mantiqueira</h3>
+                                <p className={styles.paragraphCard}>
+                                Reserva localizada na região oeste de São José dos Campos, onde possui diversos 
+                                tipos de árvores preservadas.
+                                </p>
+                            </div>
+                            <div className={styles.divButton}>
+                                <button className={styles.buttonActive} onClick={()=>alert('auu')}>Acessar</button>
+                            </div>
+                        </div>
+                    </div>    
                 </div>
             </Layout>
         </>
