5e99f358a4ed5dc41f49500f858ea6b8ede060a7
Elbert Jean <elbertjeandossantos@gmail.com>
2024-02-01 18:38:30 -0300

    feat: estilizacao card e imagens para readme

--- src/components/cardInfo/CardInfo.tsx
+++ src/components/cardInfo/CardInfo.tsx
@@ -4,7 +4,7 @@ function cardInfo():JSX.Element {
     return(
         <div className={styles.cardMap}>
             <div className={styles.cardInfo}>
-                <p className={styles.info}>Carregando informações do polígono</p>
+                <p className={styles.info}>Carregando informações da <br/>área desejada!</p>
                 <LuLoader2 className={styles.loader}/>
             </div>
         </div>
