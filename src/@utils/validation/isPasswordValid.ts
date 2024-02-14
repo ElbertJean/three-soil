d72086e7e23b91098e2762071e36f12c66f08f5e
Elbert Jean <elbertjeandossantos@gmail.com>
2024-02-14 11:43:29 -0300

    feat: Validação do login com Regex

---
+++ src/@utils/validation/isPasswordValid.ts
@@ -0,0 +1,6 @@
+function isValidatePassword(password: string): boolean {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-#!$@£%^&*()_+|~=`{}[\]:";'<>?,./])[A-Za-z\d-#!$@£%^&*()_+|~=`{}[\]:";'<>?,./]{8,}$/;
+  return passwordRegex.test(password);
+}
+
+export default isValidatePassword;
